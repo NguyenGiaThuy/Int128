a83cf3d824191ce508a4092c354680d0babc1afc
LyQuocBinh1999 <53855738+LyQuocBinh1999@users.noreply.github.com>
2020-10-21 18:39:05 +0700

    Update keyboard input

--- Calculator/UserControlKeypad.xaml.cs
+++ Calculator/UserControlKeypad.xaml.cs
@@ -1,154 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 using CLI.Int128;
+using StringToFormula;
 
 namespace Calculator
 {
     /// <summary>
     /// Interaction logic for UserControlKeypad.xaml
     /// </summary>
-    internal class StringToFormula
-    {
-        private string[] _operators = { "-", "+", "÷", "×" };
-        private Func<QInt, QInt, QInt>[] _operations = {
-            (a1, a2) => a1 - a2,
-            (a1, a2) => a1 + a2,
-            (a1, a2) => a1 / a2,
-            (a1, a2) => a1 * a2
-        };
-
-        public QInt Eval(string expression)
-        {
-            List<string> tokens = getTokens(expression);
-            Stack<QInt> operandStack = new Stack<QInt>();
-            Stack<string> operatorStack = new Stack<string>();
-            int tokenIndex = 0;
-
-            while (tokenIndex < tokens.Count)
-            {
-                string token = tokens[tokenIndex];
-                if (token == "(")
-                {
-                    string subExpr = getSubExpression(tokens, ref tokenIndex);
-                    operandStack.Push(Eval(subExpr));
-                    continue;
-                }
-                if (token == ")")
-                {
-                    throw new ArgumentException("Mis-matched parentheses in expression");
-                }
-
-                //If this is an operator  
-                if (Array.IndexOf(_operators, token) >= 0)
-                {
-                    while (operatorStack.Count > 0 && Array.IndexOf(_operators, token) < Array.IndexOf(_operators, operatorStack.Peek()))
-                    {
-                        string op = operatorStack.Pop();
-                        QInt arg2 = operandStack.Pop();
-                        QInt arg1 = operandStack.Pop();
-                        operandStack.Push(_operations[Array.IndexOf(_operators, op)](arg1, arg2));
-                    }
-                    operatorStack.Push(token);
-                }
-                else
-                {
-                    operandStack.Push(new QInt(token));
-                }
-                tokenIndex += 1;
-            }
-
-            while (operatorStack.Count > 0)
-            {
-                string op = operatorStack.Pop();
-                QInt arg2 = operandStack.Pop();
-                QInt arg1 = operandStack.Pop();
-                operandStack.Push(_operations[Array.IndexOf(_operators, op)](arg1, arg2));
-            }
-
-            return operandStack.Pop();
-        }
-
-        private string getSubExpression(List<string> tokens, ref int index)
-        {
-            StringBuilder subExpr = new StringBuilder();
-            int parenlevels = 1;
-            index += 1;
-            while (index < tokens.Count && parenlevels > 0)
-            {
-                string token = tokens[index];
-                if (tokens[index] == "(")
-                {
-                    parenlevels += 1;
-                }
-
-                if (tokens[index] == ")")
-                {
-                    parenlevels -= 1;
-                }
-
-                if (parenlevels > 0)
-                {
-                    subExpr.Append(token);
-                }
-
-                index += 1;
-            }
-
-            if ((parenlevels > 0))
-            {
-                throw new ArgumentException("Mis-matched parentheses in expression");
-            }
-
-            return subExpr.ToString();
-        }
-
-        private List<string> getTokens(string expression)
-        {
-            string operators = "()×÷+-";
-            List<string> tokens = new List<string>();
-            StringBuilder sb = new StringBuilder();
-
-            foreach (char c in expression.Replace(" ", string.Empty))
-            {
-                if (operators.IndexOf(c) >= 0)
-                {
-                    if ((sb.Length > 0))
-                    {
-                        tokens.Add(sb.ToString());
-                        sb.Length = 0;
-                    }
-                    tokens.Add(c.ToString());
-                }
-                else
-                {
-                    sb.Append(c);
-                }
-            }
-
-            if ((sb.Length > 0))
-            {
-                tokens.Add(sb.ToString());
-            }
-
-            return tokens;
-        }
-    }
-
     public partial class UserControlKeypad : UserControl
     {
         private char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        private bool operatorPressed = false;
+        private bool operatorClicked = false;
         private bool exceptionThrown = false;
-        private QInt result = new QInt("0");
+        private QInt comp = new QInt("0");
+        private QInt result;
 
         public UserControlKeypad()
         {
             InitializeComponent();
+            result = comp;
         }
 
         private void PlusMinusBtn_Click(object sender, RoutedEventArgs e)
@@ -163,35 +38,15 @@ namespace Calculator
 
             if (exceptionThrown)
             {
-                SmallTextBox.Text = "";
-                LargeTextBox.Text = "0";
-                operatorPressed = false;
-                exceptionThrown = false;
+                resetState();
             }
 
-            operatorPressed = false;
+            operatorClicked = false;
         }
 
         private void DotBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (!operatorPressed)
-            {
-                LargeTextBox.Text += (LargeTextBox.Text.Contains('.')) ? "" : ".";
-            }
-            else
-            {
-                LargeTextBox.Text = "0.";
-            }
-
-            if (exceptionThrown)
-            {
-                SmallTextBox.Text = "";
-                LargeTextBox.Text = "0";
-                operatorPressed = false;
-                exceptionThrown = false;
-            }
-
-            operatorPressed = false;
+            onDotClicked();
         }
 
         private void ZeroBtn_Click(object sender, RoutedEventArgs e)
@@ -267,50 +122,22 @@ namespace Calculator
         private void ClearEntryBtn_Click(object sender, RoutedEventArgs e)
         {
             LargeTextBox.Text = "0";
-            operatorPressed = false;
+            operatorClicked = false;
         }
 
         private void ClearBtn_Click(object sender, RoutedEventArgs e)
         {
-            SmallTextBox.Text = "";
-            LargeTextBox.Text = "0";
-            operatorPressed = false;
+            resetState();
         }
 
         private void EqualBtn_Click(object sender, RoutedEventArgs e)
         {
-            // REALLY HARD
-            if (!operatorPressed)
-            {
-                try
-                {
-                    SmallTextBox.Text += LargeTextBox.Text;
-                    StringToFormula stf = new StringToFormula();
-                    QInt temp = stf.Eval(SmallTextBox.Text);
-                    result = temp;
-                    SmallTextBox.Text += '=';
-                    LargeTextBox.Text = result.Content;
-                }
-                catch (DivideByZeroException ex)
-                {
-                    exceptionThrown = true;
-                    LargeTextBox.Text = ex.Message;
-                }
-            }
+            onEqualClicked();
         }
 
         private void BackspaceBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (LargeTextBox.Text.Length == 1 ||
-                (LargeTextBox.Text.Length == 2 &&
-                LargeTextBox.Text.Contains('-')))
-            {
-                LargeTextBox.Text = "0";
-            }
-            else if (LargeTextBox.Text.Length > 0)
-            {
-                LargeTextBox.Text = LargeTextBox.Text.Remove(LargeTextBox.Text.Length - 1);
-            }
+            onBackspaceClicked();
         }
 
         private void LargeTextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -326,31 +153,28 @@ namespace Calculator
 
         private void onOperandClicked(int num)
         {
-            if (!operatorPressed)
+            if (!operatorClicked)
             {
-                LargeTextBox.Text += num.ToString();
+                LargeTextBox.Text += (LargeTextBox.Text.Length < 40) ? num.ToString() : "";
             }
             else
             {
                 LargeTextBox.Text = num.ToString();
             }
 
-            if (result != new QInt("0"))
+            if (result != comp)
             {
                 LargeTextBox.Text = num.ToString();
                 SmallTextBox.Text = "";
-                result = new QInt("0");
+                result = comp;
             }
 
-            if(exceptionThrown)
+            if (exceptionThrown)
             {
-                SmallTextBox.Text = "";
-                LargeTextBox.Text = "0";
-                operatorPressed = false;
-                exceptionThrown = false;
+                resetState();
             }
 
-            operatorPressed = false;
+            operatorClicked = false;
         }
 
         private void onOperatorClicked(char op)
@@ -364,7 +188,7 @@ namespace Calculator
                     LargeTextBox.Text;
             }
 
-            if (!operatorPressed)
+            if (!operatorClicked)
             {
                 SmallTextBox.Text += LargeTextBox.Text + op;
             }
@@ -373,21 +197,167 @@ namespace Calculator
                 SmallTextBox.Text = SmallTextBox.Text.Remove(SmallTextBox.Text.Length - 1) + op;
             }
 
-            if (result != new QInt("0"))
+            if (result != comp)
             {
                 SmallTextBox.Text = result.Content + op;
-                result = new QInt("0");
+                result = comp;
             }
 
             if (exceptionThrown)
             {
-                SmallTextBox.Text = "";
-                LargeTextBox.Text = "0";
-                operatorPressed = false;
-                exceptionThrown = false;
+                resetState();
             }
 
-            operatorPressed = true;
+            operatorClicked = true;
+        }
+
+        private void onEqualClicked()
+        {
+            if (LargeTextBox.Text.Contains('.'))
+            {
+                LargeTextBox.Text = LargeTextBox.Text.TrimEnd('0');
+                LargeTextBox.Text =
+                    (LargeTextBox.Text.LastIndexOf('.') == LargeTextBox.Text.Length - 1) ?
+                    LargeTextBox.Text.Remove(LargeTextBox.Text.Length - 1) :
+                    LargeTextBox.Text;
+            }
+
+            if (!operatorClicked)
+            {
+                if (result == comp)
+                {
+                    try
+                    {
+                        SmallTextBox.Text += LargeTextBox.Text;
+                        QIntStringToFormula qstf = new QIntStringToFormula();
+                        QInt temp = qstf.Eval(SmallTextBox.Text);
+                        result = temp;
+                        SmallTextBox.Text += '=';
+                        LargeTextBox.Text = result.Content;
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        exceptionThrown = true;
+                        LargeTextBox.Text = ex.Message;
+                    }
+                }
+                else
+                {
+                    SmallTextBox.Text = LargeTextBox.Text + '=';
+                }
+            }
+        }
+
+        private void onBackspaceClicked()
+        {
+            if (LargeTextBox.Text.Length == 1 ||
+                (LargeTextBox.Text.Length == 2 &&
+                LargeTextBox.Text.Contains('-')))
+            {
+                LargeTextBox.Text = "0";
+            }
+            else if (LargeTextBox.Text.Length > 0)
+            {
+                LargeTextBox.Text = LargeTextBox.Text.Remove(LargeTextBox.Text.Length - 1);
+            }
+        }
+
+        private void onDotClicked()
+        {
+            if (!operatorClicked)
+            {
+                LargeTextBox.Text += (LargeTextBox.Text.Contains('.')) ? "" : ".";
+            }
+            else
+            {
+                LargeTextBox.Text = "0.";
+            }
+
+            if (exceptionThrown)
+            {
+                resetState();
+            }
+
+            operatorClicked = false;
+        }
+
+        private void resetState()
+        {
+            SmallTextBox.Text = "";
+            LargeTextBox.Text = "0";
+            operatorClicked = false;
+            exceptionThrown = false;
+            result = comp;
+        }
+
+        private void LargeTextBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            Keyboard.Focus(LargeTextBox);
+        }
+
+        private void LargeTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    onEqualClicked();
+                    break;
+                case Key.Back:
+                    onBackspaceClicked();
+                    break;
+            }
+        }
+
+        private void LargeTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.NumPad0:
+                    onOperandClicked(0);
+                    break;
+                case Key.NumPad1:
+                    onOperandClicked(1);
+                    break;
+                case Key.NumPad2:
+                    onOperandClicked(2);
+                    break;
+                case Key.NumPad3:
+                    onOperandClicked(3);
+                    break;
+                case Key.NumPad4:
+                    onOperandClicked(4);
+                    break;
+                case Key.NumPad5:
+                    onOperandClicked(5);
+                    break;
+                case Key.NumPad6:
+                    onOperandClicked(6);
+                    break;
+                case Key.NumPad7:
+                    onOperandClicked(7);
+                    break;
+                case Key.NumPad8:
+                    onOperandClicked(8);
+                    break;
+                case Key.NumPad9:
+                    onOperandClicked(9);
+                    break;
+                case Key.Add:
+                    onOperatorClicked('+');
+                    break;
+                case Key.Subtract:
+                    onOperatorClicked('-');
+                    break;
+                case Key.Multiply:
+                    onOperatorClicked('×');
+                    break;
+                case Key.Divide:
+                    onOperatorClicked('÷');
+                    break;
+                case Key.Decimal:
+                    onDotClicked();
+                    break;
+            }
         }
     }
 }
---
+++ StringToFormula/QIntToFormula.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CLI.Int128;
+
+namespace StringToFormula
+{
+    public class QIntStringToFormula
+    {
+        private string[] _operators = { "-", "+", "÷", "×" };
+        private Func<QInt, QInt, QInt>[] _operations = {
+            (a1, a2) => a1 - a2,
+            (a1, a2) => a1 + a2,
+            (a1, a2) => a1 / a2,
+            (a1, a2) => a1 * a2
+        };
+
+        public QInt Eval(string expression)
+        {
+            List<string> tokens = getTokens(expression);
+            Stack<QInt> operandStack = new Stack<QInt>();
+            Stack<string> operatorStack = new Stack<string>();
+            int tokenIndex = 0;
+
+            while (tokenIndex < tokens.Count)
+            {
+                string token = tokens[tokenIndex];
+                if (token == "(")
+                {
+                    string subExpr = getSubExpression(tokens, ref tokenIndex);
+                    operandStack.Push(Eval(subExpr));
+                    continue;
+                }
+                if (token == ")")
+                {
+                    throw new ArgumentException("Mis-matched parentheses in expression");
+                }
+
+                //If this is an operator  
+                if (Array.IndexOf(_operators, token) >= 0)
+                {
+                    while (operatorStack.Count > 0 && Array.IndexOf(_operators, token) < Array.IndexOf(_operators, operatorStack.Peek()))
+                    {
+                        string op = operatorStack.Pop();
+                        QInt arg2 = operandStack.Pop();
+                        QInt arg1 = operandStack.Pop();
+                        operandStack.Push(_operations[Array.IndexOf(_operators, op)](arg1, arg2));
+                    }
+                    operatorStack.Push(token);
+                }
+                else
+                {
+                    operandStack.Push(new QInt(token));
+                }
+                tokenIndex += 1;
+            }
+
+            while (operatorStack.Count > 0)
+            {
+                string op = operatorStack.Pop();
+                QInt arg2 = operandStack.Pop();
+                QInt arg1 = operandStack.Pop();
+                operandStack.Push(_operations[Array.IndexOf(_operators, op)](arg1, arg2));
+            }
+
+            return operandStack.Pop();
+        }
+
+        private string getSubExpression(List<string> tokens, ref int index)
+        {
+            StringBuilder subExpr = new StringBuilder();
+            int parenlevels = 1;
+            index += 1;
+            while (index < tokens.Count && parenlevels > 0)
+            {
+                string token = tokens[index];
+                if (tokens[index] == "(")
+                {
+                    parenlevels += 1;
+                }
+
+                if (tokens[index] == ")")
+                {
+                    parenlevels -= 1;
+                }
+
+                if (parenlevels > 0)
+                {
+                    subExpr.Append(token);
+                }
+
+                index += 1;
+            }
+
+            if ((parenlevels > 0))
+            {
+                throw new ArgumentException("Mis-matched parentheses in expression");
+            }
+
+            return subExpr.ToString();
+        }
+
+        private List<string> getTokens(string expression)
+        {
+            string operators = "()×÷+-";
+            List<string> tokens = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in expression.Replace(" ", string.Empty))
+            {
+                if (operators.IndexOf(c) >= 0)
+                {
+                    if ((sb.Length > 0))
+                    {
+                        tokens.Add(sb.ToString());
+                        sb.Length = 0;
+                    }
+                    tokens.Add(c.ToString());
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            if ((sb.Length > 0))
+            {
+                tokens.Add(sb.ToString());
+            }
+
+            return tokens;
+        }
+    }
+}
